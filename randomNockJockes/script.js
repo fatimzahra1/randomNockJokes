87cd166309717351bca2eca3643ead9d0c628aa0
fatimzahra1 <fatimzahraelmadini@gmail.com>
2021-10-05 16:13:04 +0200

    Fix bugs

--- randomNockJockes/script.js
+++ randomNockJockes/script.js
@@ -4,7 +4,7 @@ message.content = ["Who's there? Says. Says who? Says me, that's who!",
     "Who's there? Europe. Europe who? No, YOU'RE a poo!",
     "Who's there? Ruff ruff. Ruff ruff who? Who let the dogs out? I heard barking!",
     "Who's there? Weekend. Weekend who? Weekend do anything we want!",
-    " Who's there? Hawaii. Hawaii who? I'm fine, Hawaii you?", " Who's there? Nana. Nana who? Nana your business.",
+    " Who's there? Hawaii. Hawaii who? I'm fine, Hawaii you?", " Who's there? Nanna Nanna who? Nanna your business",
     "Who's there? Adore. Adore who? Adore is between us. Open up!"
 ]
 
